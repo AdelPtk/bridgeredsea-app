c5931a1294b6f3c4fd1cd9f51f2f94524e4bc701
unknown <liapet1212@gmail.com>
2025-08-01 19:17:01 +0300

    alon2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ? '/' : '/bridgeredsea-app/',
+  base: '/bridgeredsea-app/',
   server: {
     host: "::",
     port: 8080,
